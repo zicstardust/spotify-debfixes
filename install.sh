e9fa7c68f789b879a178060d84562096e9d252f5
Zic <git@zicstardust.com>
2026-02-27 08:51:52 -0400

    fix spotify bin

--- install.sh
+++ install.sh
@@ -51,15 +51,15 @@ fi
 mapfile -t FLAGS <<< "$(grep -v -E '^\s*$|^#' "${HOME}/.config/spotify/spotify-flags.conf")"
 mapfile -t ENVS <<< "$(grep -v -E '^\s*$|^#' "${HOME}/.config/spotify/spotify.env")"
 
-if [[ -z \${ENVS[0]} ]]; then
-    exec /usr/share/spotify/spotify \\
-        "\${FLAGS[@]}" \\
-        "\$@"
+if [[ -z ${ENVS[0]} ]]; then
+    exec /usr/share/spotify/spotify \
+        "${FLAGS[@]}" \
+        "$@"
 else
-    exec env "\${ENVS[@]}" \\
-        /usr/share/spotify/spotify \\
-        "\${FLAGS[@]}" \\
-        "\$@"
+    exec env "${ENVS[@]}" \
+        /usr/share/spotify/spotify \
+        "${FLAGS[@]}" \
+        "$@"
 fi
 
 EXEC
