dc1c437fd9cd9483856342d4cf384943ce02b904
Zic <git@zicstardust.com>
2026-03-12 15:47:06 -0400

    fix: uncomment uninstall script logic in hook script

--- src/spotifydebfixes-hook.sh
+++ src/spotifydebfixes-hook.sh
@@ -5,4 +5,7 @@ PKG="spotify-client"
 if grep -q "install $PKG" /var/log/dpkg.log || grep -q "upgrade $PKG" /var/log/dpkg.log; then
     /usr/local/bin/spotify-debfixes
     #echo "Spotify DEB Fixes installed!"
+#elif grep -q "remove $PKG" /var/log/dpkg.log || grep -q "purge $PKG" /var/log/dpkg.log; then
+#    /usr/local/bin/spotify-debfixes --uninstall
+    #echo "Spotify DEB Fixes uninstalled!"
 fi
